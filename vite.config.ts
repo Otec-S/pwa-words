7819a10679e8122a99116dd3be3a1d84248cb95a
Sergey Grigorash <grigorash.s@brainrocket.com>
2025-12-28 13:39:17 +0500

    fix(vite): correct icon paths in PWA configuration for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -25,16 +25,28 @@ export default defineConfig({
         scope: '/pwa-words/',
         icons: [
           {
-            src: 'icons/icon-192x192.png',
+            src: '/pwa-words/icons/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: '/pwa-words/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: '/pwa-words/icons/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
+          },
+          {
+            src: '/pwa-words/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
         ]
       },
